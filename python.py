687620e406efb3bda300c53fa11d538a57cb26c0
Charlotte Law <69742430+CharlotteLaw@users.noreply.github.com>
2023-06-08 16:17:31 +0800

    Allows users to see their previous message
    Touched up MYSQL database integration

--- python.py
+++ python.py
@@ -1,6 +1,8 @@
 import openai
 from flask import Flask, render_template, request
 import mysql.connector
+from html import unescape
+
 #from api_secrets import API_KEY
 
 app = Flask(__name__)
@@ -8,19 +10,16 @@ app = Flask(__name__)
 db = mysql.connector.connect(
     host="localhost",
     user="root",
-    password="Charlotte19!",
-    database="messages"
+    password="xxx", #Use your own MySQL password.
+    database="messages" 
 )
 
 mycursor = db.cursor()
 
-mycursor.execute("SELECT * FROM Conversations")
-
-for x in mycursor:
-    print(x)
-
+#Create your own database and table in MySQL Workbench.
 #mycursor.execute("CREATE DATABASE messages")
-#mycursor.execute("CREATE TABLE Conversations (question VARCHAR(300), response VARCHAR(300))")
+#mycursor.execute("CREATE TABLE convo (ID int primary key, question VARCHAR(3000), response VARCHAR(3000))")
+#set ID to auto increment
 
 @app.route('/')
 def index():
@@ -28,15 +27,30 @@ def index():
 
 @app.route('/', methods=['POST'])
 def getvalue():
+    
+    mycursor.execute("SELECT question FROM convo ORDER BY ID DESC LIMIT 1")
+    qu = str(mycursor.fetchall())
+    qu = qu.replace("[('", '')
+    qu = qu.replace("',)]", '')
+
+    mycursor.execute("SELECT response FROM convo ORDER BY ID DESC LIMIT 1")
+    re = str(mycursor.fetchall())
+    re = re.replace("[('", '')
+    re = re.replace('[("', '')
+    re = re.replace("',)]", '')
+    re = re.replace('",)]', '')
+    num_appear = re.count('\\n')
+    re = re.replace("\\n", '', num_appear)
+
     message = request.form['message']
-    askprompt = "You are an informative, happy, and helpful personal assistant/ AI chatbot. Chat with the user or answer their questions. The user says: " + message
-    openai.api_key = "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"
+    askprompt = "You are an informative, happy, and helpful personal assistant/ AI chatbot. Chat with the user or answer their questions. The conversation starts now. " + message
+    openai.api_key = "xxx" #generate your own API key in OpenAi.
     response = openai.Completion.create(engine="text-davinci-001", prompt=askprompt, max_tokens=200)
     resp = response.choices[0].text
     print(response)
-    mycursor.execute("INSERT INTO Conversations (question, response) VALUES (%s, %s)", (message, resp))
+    mycursor.execute("INSERT INTO convo (question, response) VALUES (%s, %s)", (message, resp))
     db.commit()
-    return render_template('index.html', m=message, r=resp)
+    return render_template('index.html', m=message, r=resp, n=qu, s=re)
 
 
 if __name__ == '__main__':
